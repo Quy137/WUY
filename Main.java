b9a9db3b203c72e9356c9143d5265c256f413c0c
Quy137 <124586880+Quy137@users.noreply.github.com>
2023-03-18 01:14:57 +0700

    Lab6B

--- Main.java
+++ Main.java
@@ -1,41 +1,37 @@
 
-package lab6;
+package lab6b;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        NhanVien nv = new NhanVien();
-        NhanVienPartTime nvpt = new NhanVienPartTime();
-        NhanVienFullTime nvft = new NhanVienFullTime();
-        System.out.println("Nhap ten nhan vien");
-        nv.setTen(input.nextLine());
-        System.out.println("Nhap luong");
-        nv.setLuong(input.nextLong());
-        nv.xuatThongTin();
+        Scanner input =  new Scanner(System.in);
+        HocSinh hs = new HocSinh();
+        HocSinhChuyenToan  hst = new HocSinhChuyenToan();
+        System.out.println("Nhap ten hoc sinh");
+        hs.setHoTen(input.nextLine());
+        System.out.println("Nhap lop");
+        hs.setLop(input.nextInt());
+        System.out.println("Nhap diem mon toan");
+        hs.setToan(input.nextFloat());
+        System.out.println("Nhap diem mon ly");
+        hs.setLy(input.nextFloat());
+        System.out.println("Nhap diem mon hoa");
+        hs.setHoa(input.nextFloat());
+        hs.diemTrungBinh();
         
-        System.out.println("Nhap ten nhan vien");
-        nvpt.setTen(input.nextLine());
-        nvpt.setTen(input.nextLine());
-        System.out.println("Nhap luong");
-        nvpt.setLuong(input.nextLong());
-        System.out.println("Nhap gio lam viec");
-        nvpt.setGioLamViec(input.nextInt());
-        nvpt.loaiNhanVien();
-        nvpt.tinhLuong();
-        
-        
-        System.out.println("Nhap ten nhan vien");
-        nvft.setTen(input.nextLine());
-        nvft.setTen(input.nextLine());
-        System.out.println("Nhap luong");
-        nvft.setLuong(input.nextLong());
-        System.out.println("Nhap ngay lam viec");
-        nvft.setNgayLamThem(input.nextInt());
-        nvft.loaiNhanVien();
-        nvft.tinhLuong();
-        
+        System.out.println("\nNhap ten hoc sinh");
+        hst.setHoTen(input.nextLine());
+        hst.setHoTen(input.nextLine());
+        System.out.println("Nhap lop");
+        hst.setLop(input.nextInt());
+        System.out.println("Nhap diem mon toan");
+        hst.setToan(input.nextFloat());
+        System.out.println("Nhap diem mon ly");
+        hst.setLy(input.nextFloat());
+        System.out.println("Nhap diem mon hoa");
+        hst.setHoa(input.nextFloat());
+        hst.diemTrungBinh();
         
         
     }
